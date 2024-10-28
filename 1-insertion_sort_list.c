9eb74a78b25a810dbd1b312b61c5e2c0d4832f3b
medayoubadri <medayoubadri@gmail.com>
2024-10-28 14:39:11 +0100

    1. Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the head of the list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list in ascending order
+ *                       using the Insertion Sort algorithm.
+ * @list: Pointer to the head of the list
+ *
+ * Description: A classic sort where each element is inserted into its
+ * correct place
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			swap_nodes(list, temp->prev, temp);
+			print_list(*list);
+		}
+	}
+}
